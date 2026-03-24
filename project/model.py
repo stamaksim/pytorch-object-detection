f4715479f782aab0dd1d1072e69c64f022d6b8f9
Rohit Gadgil <sc15527@student.samk.fi>
2026-03-24 16:17:48 +0200

    model

--- project/model.py
+++ project/model.py
@@ -12,7 +12,7 @@ def build_model(backbone: str, num_classes: int):
         weights = torchvision.models.detection.FasterRCNN_MobileNet_V3_Large_FPN_Weights 
         model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn (pretrained=True, weights=weights)
 
-    #in_feature = model.roi_heads.box_predictor.cls_score.in_features
-    #model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, num_classes=num_classes)
+    in_feature = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, num_classes=num_classes)
 
     return model, 
