b11f3eba87545e84386a111eb75e0c802df621a2
Rohit Gadgil <sc15527@student.samk.fi>
2026-03-24 16:12:47 +0200

    finishing the project

--- project/main.py
+++ project/main.py
@@ -1,9 +1,11 @@
 from args import get_args
 import pandas as pd
 import os
+import torch
 from torch.utils.data import DataLoader
 from dataset import ObjDetectionDataset
 from model import build_model
+from trainer import train_model
 
 def collate(batch):
     images, targets = zip(*batch)
@@ -11,7 +13,8 @@ def collate(batch):
     return list(images), list(targets)
 def main():
     args = get_args()
-    
+
+
     train_df = pd.read_csv (os.path.join(args.csv_dir, 'train_df.csv'))
     val_df = pd.read_csv (os.path.join(args.csv_dir, 'val_df.csv'))
     
@@ -22,18 +25,27 @@ def main():
         train_dataset,
         batch_size=args.batch_size,
         shuffle=True,
-        collate_fn=collate
+        collate_fn=collate,
+        num_workers=0,
+        pin_memory=  torch.cuda.is_available()
         )
 
     val_loader = DataLoader(
         val_dataset,
         batch_size=args.batch_size,
         shuffle=False,
-        collate_fn=collate
+        collate_fn=collate,
+        num_workers=0,
+        pin_memory=  torch.cuda.is_available()
         )
     
     #images, targets = next(iter(train_loader))
-    model = build_model(args.backbone)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = build_model(args.backbone, num_classes=args.num_classes + 1)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_model(model, train_loader, val_loader, torch.device)
 
     print()
 
--- project/model.py
+++ project/model.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
-def build_model(backbone: str):
+def build_model(backbone: str, num_classes: int):
     if backbone == 'fasterrcnn_resnet50_fpn':
         weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_Weights
         model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True, weights=weights)
@@ -11,5 +11,8 @@ def build_model(backbone: str):
     else:
         weights = torchvision.models.detection.FasterRCNN_MobileNet_V3_Large_FPN_Weights 
         model = torchvision.models.detection.fasterrcnn_mobilenet_v3_large_fpn (pretrained=True, weights=weights)
+
+    #in_feature = model.roi_heads.box_predictor.cls_score.in_features
+    #model.roi_heads.box_predictor = FastRCNNPredictor(in_feature, num_classes=num_classes)
 
     return model, 
