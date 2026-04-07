ca323b1547606dad821c3aa677e208c8bd56d1ef
Rohit Gadgil <sc15527@student.samk.fi>
2026-04-07 22:53:45 +0300

    Add --image_size argument to args.py
    Add image_size argument to argument parser

--- project/args.py
+++ project/args.py
@@ -8,6 +8,8 @@ def get_args():
                         choices=['fasterrcnn_resnet50_fpn', 'fasterrcnn_mobilenet_v3'])
     
     parser.add_argument("--num_classes", type=int, default=1)
+    parser.add_argument("--image_size", type=int, default=512)
+
 
     parser.add_argument("--csv_dir", type=str, default="data/CSVs")
     parser.add_argument("--outdir", type=str, default="output")
