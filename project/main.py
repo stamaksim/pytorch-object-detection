aec547e26d4eb1c360b67f04026785e612617f27
Rohit Gadgil <sc15527@student.samk.fi>
2026-04-07 22:52:58 +0300

    Add data augmentation transforms to datasets

--- project/main.py
+++ project/main.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from dataset import ObjDetectionDataset
 from model import build_model
 from trainer import train_model
+from augmentations import build_train_transforms, build_val_transforms
 
 def collate(batch):
     images, targets = zip(*batch)
@@ -18,8 +19,10 @@ def main():
     train_df = pd.read_csv (os.path.join(args.csv_dir, 'train_df.csv'))
     val_df = pd.read_csv (os.path.join(args.csv_dir, 'val_df.csv'))
     
-    train_dataset = ObjDetectionDataset(train_df)
-    val_dataset = ObjDetectionDataset(val_df)
+    train_dataset = ObjDetectionDataset(train_df, 
+                                        transforms = build_train_transforms(args.image_size))
+    val_dataset = ObjDetectionDataset(val_df, 
+                                      transforms = build_val_transforms(args.image_size))
     
     train_loader = DataLoader(
         train_dataset,
@@ -27,7 +30,7 @@ def main():
         shuffle=True,
         collate_fn=collate,
         num_workers=0,
-        pin_memory=  torch.cuda.is_available()
+        pin_memory=  torch.cuda.is_available(),
         )
 
     val_loader = DataLoader(
@@ -36,18 +39,17 @@ def main():
         shuffle=False,
         collate_fn=collate,
         num_workers=0,
-        pin_memory=  torch.cuda.is_available()
+        pin_memory=  torch.cuda.is_available(),
         )
     
     #images, targets = next(iter(train_loader))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     model = build_model(args.backbone, num_classes=args.num_classes + 1)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    train_model(model, train_loader, val_loader, torch.device)
+   
+    train_model(model, train_loader, val_loader, device)
 
-    print()
 
 if __name__ == "__main__":
     main()  
