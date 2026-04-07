244883454f11f72949e2274f8a909ce9c5b42bd8
Rohit Gadgil <sc15527@student.samk.fi>
2026-04-07 22:54:25 +0300

    Enhance ObjDetectionDataset with transformations
    Added image resizing and transformation functionality to the dataset class.

--- project/dataset.py
+++ project/dataset.py
@@ -1,33 +1,50 @@
 import torch 
 from PIL import Image
 from torchvision.transforms.functional import to_tensor
+from args import get_args
+from utils import resize_box_xyxy
+import augmentations as aug
+
 
 
 class ObjDetectionDataset(torch.utils.data.Dataset):
-    def __init__(self, df):
+    def __init__(self, df, transform=None):
         self.df = df.reset_index(drop=True)
+
+        if transform is None:
+            self.transform = aug.NoTransform()
+
+        else:
+            self.transform = aug.NoTransform()
+            
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
+        args= get_args()
         # TODO 1: Get the row number idx from dataframe
         # your code here
         
         row = self.df.iloc[idx]
 
-        img = Image.open(row["image_path"]).convert("RGB")
+        img = Image.open(row["images"]).convert("RGB")
         w, h = img.size
+
+        img = img.resize((args.image_size, args.image_size))
+
         image = to_tensor(img)
 
         boxes, labels = [], []
-        with open(row["label_path"]) as f:
+        with open(row["labels"]) as f:
             for line in f:
                 cls, xc, yc, bw, bh = map(float, line.split())
                 x1 = (xc - bw/2) * w
                 y1 = (yc - bh/2) * h
                 x2 = (xc + bw/2) * w
                 y2 = (yc + bh/2) * h
+
+                x1, y1, x2, y2 = resize_box_xyxy((x1, y1, x2, y2), w, h, args.image_size, args.image_size)
                 boxes.append([x1, y1, x2, y2])
                 labels.append(int(cls) + 1)
 
@@ -38,5 +55,7 @@ class ObjDetectionDataset(torch.utils.data.Dataset):
         }
         # TODO 2: Return what you need from this class
         # your code here
+        image, target = self.transform(image, target)
 
         return image, target
+    
